8ce0766baad9c05a5bafd9fb53609807a792fd7d
Michal Andros <michalandros@gmail.com>
2016-09-10 22:14:14 +0200

    made sure bootstrap works, changed form to work with simple form

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  expose :articles, ->{ Article.all }
+  expose :article
+
+  def create
+    if article.save
+      redirect_to article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if article.update(article_params)
+      redirect_to article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    article.destroy
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :published_at, :avatar)
+  end
+end
